33fe805c589b897bb0c9899785bb2e2bef6b0bc5
Deivid de Oliveira Raimundo <deivid.raimundo@aswti.com.br>
2022-07-06 11:34:53 -0300

    Subindo mais dois capitulos do curso (concorrencia e testes)

---
+++ testes/matematica/matematica.go
@@ -0,0 +1,19 @@
+package matematica
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// teste unitário é testar uma unida (Exemplo: uma função)
+
+// Media é responsável por calcular o que você já sabe... :)
+func Media(numeros ...float64) float64 {
+	total := 0.0
+	for _, num := range numeros {
+		total += num
+	}
+	media := total / float64(len(numeros))
+	mediaArredondada, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", media), 64)
+	return mediaArredondada
+}
